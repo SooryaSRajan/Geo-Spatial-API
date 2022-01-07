17b8db77d89b053195e4cface913ba766f4bdc3e
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-01-08 00:52:45 +0530

    added user-info route

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -3,12 +3,10 @@ const router = Express.Router();
 
 const mongoose = require("mongoose");
 const UserModel = require("../Models/users");
-const AuthMobile = require('../Middleware/AuthMobile');
+const AuthMobile = require("../Middleware/AuthMobile");
 
-router.get("/",AuthMobile,async (request, response) => {
-    const UserInfo = await UserModel.find({ username: request.user.username });
-    response.status.(200).send(UserInfo);
-})
+router.get("/", AuthMobile, async (request, response) => {
+  const UserInfo = await UserModel.find({ username: request.user.username });
+  response.status(200).send(UserInfo);
+});
 module.exports = router;
-
-
