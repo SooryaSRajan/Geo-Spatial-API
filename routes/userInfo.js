4a261be5821d97d5d01ff0476a2220f0892d3717
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-01-08 01:00:11 +0530

    bug fixes

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -1,12 +1,25 @@
 const Express = require("express");
 const router = Express.Router();
-
+const _ = require("lodash");
 const mongoose = require("mongoose");
 const { User } = require("../Models/users");
 const AuthMobile = require("../Middleware/AuthMobile");
 
 router.get("/", AuthMobile, async (request, response) => {
   const UserInfo = await User.find({ username: request.user.username });
-  response.status(200).send(UserInfo);
+
+  var UserInfoList = [];
+  UserInfo.forEach((user) => {
+    UserInfoList.push(
+      _.pick(user, [
+        "Name",
+        "username",
+        "gender",
+        "DateJoined",
+        "NumberOfRecordsCollected",
+      ])
+    );
+  });
+  response.status(200).send(UserInfoList);
 });
 module.exports = router;
