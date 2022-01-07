dcbe07da97a3f4c689a6c9264cd5b11b3f10971c
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-01-08 01:20:03 +0530

    added password change feature

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -4,6 +4,7 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 const { User } = require("../Models/users");
 const AuthMobile = require("../Middleware/AuthMobile");
+const bcrypt = require("bcrypt");
 
 router.get("/", AuthMobile, async (request, response) => {
   const UserInfo = await User.find({ username: request.user.username });
@@ -22,4 +23,29 @@ router.get("/", AuthMobile, async (request, response) => {
   });
   response.status(200).send(UserInfoList);
 });
+
+router.put(
+  "/changePassword/:username",
+  AuthMobile,
+  async (request, response) => {
+    var username = request.params.username;
+    var newpassword = request.body.password;
+
+    const salt = await bcrypt.genSalt(10);
+    const HashedPassword = await bcrypt.hash(newpassword, salt);
+
+    const UserInstance = await User.findOneAndUpdate(
+      { username: username },
+      { password: HashedPassword },
+      null,
+      (err, doc) => {
+        if (error) {
+          return response.status(406).send(error);
+        } else {
+          return response.status(200).send("Password Changed Successfully");
+        }
+      }
+    );
+  }
+);
 module.exports = router;
