8a46add8f7fe7b4980ccf2e82e042466a53f12f9
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-01-19 23:21:16 +0530

    added a function to convert mongo date format to js date format (DD/MM/YYYY)

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -5,6 +5,12 @@ const mongoose = require("mongoose");
 const { User } = require("../Models/users");
 const AuthMobile = require("../Middleware/AuthMobile");
 const bcrypt = require("bcrypt");
+const moment = require("moment");
+
+function formatDate(date) {
+  // "2022-01-07T18:08:57.250Z" -- sample
+  return String(moment(date).format("YYYY-MM-DD"));
+}
 
 router.get("/", AuthMobile, async (request, response) => {
   const UserInfo = await User.find({ username: request.user.username });
@@ -20,6 +26,12 @@ router.get("/", AuthMobile, async (request, response) => {
         "NumberOfRecordsCollected",
       ])
     );
+
+    console.log(UserInfoList);
+    UserInfoList.forEach((user) => {
+      user.DateJoined = formatDate(user.DateJoined);
+    });
+    console.log(UserInfoList);
   });
   response.status(200).send(UserInfoList);
 });
