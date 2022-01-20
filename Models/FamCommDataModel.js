15de0c732520447c1a05733cb13aaaf4a891c626
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-01-21 01:58:51 +0530

    minor bug fixes

--- Models/FamCommDataModel.js
+++ Models/FamCommDataModel.js
@@ -1,7 +1,41 @@
 const mongoose = require("mongoose");
-const { personalDataSchema } = require("./personalDataSchema");
 
-console.log(personalDataSchema);
+const personalDataSchema = new mongoose.Schema({
+  UIN: String,
+  name: String,
+  dateOfBirth: String,
+  gender: String,
+  phoneNumber: String,
+  educationalQualification: String,
+  aadhaarNumber: String,
+  Vulnerabilities: [String],
+  isADailyWageWorker: String,
+  occupationData: [occupationDataSchema],
+  employed: String,
+  income: String,
+  incomeType: String,
+  oldAgePension: String,
+  businessStatus: String,
+  maritalStatus: String,
+  noOfDaysWorking: String,
+  specialSkills: [String],
+  workTimings: [String],
+  surgeriesUndergone: String,
+  anganwadiServicesAware: String,
+  anganwadiServicesUsed: String,
+  anganwadiServicesUtilised: [String],
+  phcServicesUtilised: String,
+  privateHealthClinicFacilitiesUsed: [String],
+  reasonsForVisitingPrivateHealthClinic: [String],
+  communicableDiseases: [String],
+  frequentHealthAilments: [String],
+  nonCommunicableDiseases: [String],
+  tobaccoBasedProductsUsage: String,
+  tobaccoProductsUsed: [String],
+  alcoholConsumption: String,
+  arogyaSethuAppInstallationStatus: String,
+  vizhithiruAppInstallationStatus: String,
+});
 
 const FamilyindData = new mongoose.Schema({
   familyMemberData: [personalDataSchema],
