34ce2452354ba509d0f3c2847884895476049503
sooryasrajan <soorya.s27@gmail.com>
2022-07-23 15:18:08 +0530

    Added new route to get village data

--- routes/GetCommonRecords.js
+++ routes/GetCommonRecords.js
@@ -2,15 +2,26 @@ const Express = require("express");
 const AuthMobile = require("../Middleware/AuthMobile");
 const FamilyDataModel = require("../Models/FamCommDataModel");
 const router = Express.Router();
+const VillageModel = require("../Models/villageInfo");
 
-
-router.get("/", AuthMobile, async (request, response) => {
+router.get("/familyData", AuthMobile, async (request, response) => {
     console.log(request.user.username)
     FamilyDataModel.find({volunteerUserId: request.user.username}, function (err, user) {
         if (!err) {
             response.status(200).send(user);
+        } else {
+            response.status(404).send("Data not found");
         }
-        else {
+    });
+
+});
+
+router.get("/individualData", AuthMobile, async (request, response) => {
+    console.log(request.user.username)
+    VillageModel.find({volunteerUserId: request.user.username}, function (err, user) {
+        if (!err) {
+            response.status(200).send(user);
+        } else {
             response.status(404).send("Data not found");
         }
     });
